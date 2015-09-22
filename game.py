5c78ddde3dffec0218395ff824ab99ed56e2fdf8
Hackbright Student <info@hackbrightacademy.com>
2015-09-22 16:14:34 -0700

    get user guess, react

--- game.py
+++ game.py
@@ -3,5 +3,16 @@ import random
 print "Welcome to our guessing game!"
 print "What is your name?"
 user_name = raw_input(">")
+
 secret_number = random.randint(1,100)
-print secret_number
+print secret_number #TAKE THIS OUT
+
+while True:
+    user_guess = int(raw_input("Guess a number: "))
+    if user_guess > secret_number:
+        print "Too high"
+    elif user_guess < secret_number:
+        print "Too low"
+    else: 
+        print "You guessed it!"
+        break
